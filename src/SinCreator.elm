be68e5bf3aa9b502f2555cfdc14e4abbbf610d99
Emily Hodder <hoddere@mcmaster.ca>
2019-11-01 18:22:04 -0400

    Stuff

--- src/SinCreator.elm
+++ src/SinCreator.elm
@@ -1,31 +1,17 @@
 module SinCreator exposing (..)
 
 {-
-Copyright 2017-2019 Christopher Kumar Anand,  Adele Olejarz, Chinmay Sheth, Yaminah Qureshi, Graeme Crawley and students of McMaster University.  Based on the Shape Creator by Levin Noronha.
+Emily Hodder - 400008260
+Evan Kieft - 001315430
 
-   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-   1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
-   2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution, and cite the paper
-
-   @article{d_Alves_2018,
-   title={Using Elm to Introduce Algebraic Thinking to K-8 Students},
-   volume={270},
-   ISSN={2075-2180},
-   url={http://dx.doi.org/10.4204/EPTCS.270.2},
-   DOI={10.4204/eptcs.270.2},
-   journal={Electronic Proceedings in Theoretical Computer Science},
-   publisher={Open Publishing Association},
-   author={d’ Alves, Curtis and Bouman, Tanya and Schankula, Christopher and Hogg, Jenell and Noronha, Levin and Horsman, Emily and Siddiqui, Rumsha and Anand, Christopher Kumar},
-   year={2018},
-   month={May},
-   pages={18–36}
-   }
-
-   3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR AN, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+User: Who is your imagined user
+Activity: What activity is your user engaged in
+Emotion: How does your user feel about this activity
+Tasks: What tasks are part of this activity
+Typical Interaction: Describe a typical interaction 
+    of your user (the TAs will test this out, so include sufficient detail)
+Principle 1: First of Norman's principles and how it guided your design
+Principle 2: Second of Norman's principles and how it guided your design
 
 -}
 
